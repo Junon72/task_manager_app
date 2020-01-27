530610a965ce69c2b4e4e6a3c947bbed4d825bc1
Jussi Nousiainen <juno.athome@gmail.com>
2020-01-27 15:26:46 +0100

    reverted to original code. env.py URI configuration did not work as expected.

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from os import path
-if path.exists("env.py"):
-      import env
+from bson.objectid import ObjectId 
 
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-MONGODB_URI = os.getenv('MONGO_URI')
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tU5er@myfirstcluster-i0x5z.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
